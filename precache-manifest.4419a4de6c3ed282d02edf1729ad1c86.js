4bce59a4256841fb848026518e5118fedb38e7d0
aulb <aulbert@aulb.local>
2019-02-17 23:30:34 -0800

    new demo

---
+++ precache-manifest.4419a4de6c3ed282d02edf1729ad1c86.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "7dcada5a0a49c2182529",
+    "url": "/static/js/main.7dcada5a.chunk.js"
+  },
+  {
+    "revision": "aa10e0d0b565a769325d",
+    "url": "/static/js/2.aa10e0d0.chunk.js"
+  },
+  {
+    "revision": "7dcada5a0a49c2182529",
+    "url": "/static/css/main.61f0506c.chunk.css"
+  },
+  {
+    "revision": "3d64de348f98ae47f940a27e48e9d0de",
+    "url": "/index.html"
+  }
+];
